c7de0ec05ddb88225d7939f5cd4bc1534d9ddd08
Nirzvi1 <guys@qualum.com>
2018-06-28 09:31:25 -0400

    Added translator for Vec3 to Twist for navsat/vel

---
+++ heron_gazebo/scripts/navsat_vel_translate.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# Software License Agreement (BSD)
+#
+# @author    Nirzvi1 <guys@qualum.com>
+# @copyright (c) 2018, Clearpath Robotics, Inc., All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without modification, are permitted provided that
+# the following conditions are met:
+# * Redistributions of source code must retain the above copyright notice, this list of conditions and the
+#   following disclaimer.
+# * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the
+#   following disclaimer in the documentation and/or other materials provided with the distribution.
+# * Neither the name of Clearpath Robotics nor the names of its contributors may be used to endorse or
+#   promote products derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+# WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+# PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+# TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+import rospy
+from geometry_msgs.msg import TwistStamped
+from geometry_msgs.msg import Vector3Stamped
+from geometry_msgs.msg import Vector3
+
+def vec_cb(msg):
+    global twist_pub
+
+    twst = TwistStamped()
+    twst.header = msg.header
+    twst.twist.linear = msg.vector
+    twst.twist.angular = Vector3(0, 0, 0)
+
+    twist_pub.publish(twst)
+
+def translate_navvel():
+    global twist_pub
+
+    rospy.init_node("navvel_translator")
+
+    vec_sub = rospy.Subscriber("navsat/velocity", Vector3Stamped, vec_cb)
+    twist_pub = rospy.Publisher("navsat/vel", TwistStamped, queue_size=1)
+
+    rospy.spin()
+
+if __name__ == '__main__':
+    translate_navvel()
